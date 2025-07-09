aa6380cea22a2f82b1ad3f5a352f92990112f47a
Bruno Cesar <brunooufrn@gmail.com>
2025-07-09 05:23:46 -0300

    refactor: init profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Box, Container, Typography } from "@mui/material";
 
 export const ProfilePage = ({ pokemonData }) => {
+  const { name, sprites } = pokemonData;
   console.log(pokemonData);
   return (
     <>
-      {" "}
       <Navbar hideSearch />
+      <Container maxWidth="md">
+        <Typography variant="h5" component="h1" gutterBottom>
+          {name}
+        </Typography>
+        <Box component="img" src={sprites?.front_default} width="50%" />
+      </Container>
     </>
   );
 };
