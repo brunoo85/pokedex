4bddadc743f57689af143684b249cd29586e109e
Bruno Cesar <brunooufrn@gmail.com>
2025-07-16 05:11:52 -0300

    wip: div behind pokemon

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -20,11 +20,28 @@ export default function PokemonCard({ pokemon }) {
       }}
       className="cardPokemon"
     >
-      <CardMedia
-        sx={{ height: 200 }}
-        image={`${sprites.other["official-artwork"].front_default}`}
-        title="green iguana"
-      />
+      <Box
+        height={200}
+        display={"flex"}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          display="block"
+          width="40%"
+          height="35%"
+          border-radius="58% 42% 64% 36% / 47% 45% 55% 53%"
+          background-color="red"
+          position="absolute"
+          top="25%"
+          left="30%"
+        />
+        <CardMedia
+          sx={{ height: 150, width: 150, position: "relative" }}
+          image={`${sprites.other["official-artwork"].front_default}`}
+          title="green iguana"
+        />
+      </Box>
       <CardContent>
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <Typography gutterBottom variant="h5" component="div">
