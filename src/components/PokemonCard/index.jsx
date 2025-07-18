5b9bd06ce1b97280208602dd0acd3c6a73f66ac1
Bruno <brunooufrn@gmail.com>
2025-07-18 08:03:31 -0300

    chore: div appears

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -12,7 +12,7 @@ export default function PokemonCard({ pokemon }) {
 
   return (
     <Card
-      sx={{ maxWidth: 360, width: 250 }}
+      sx={{ maxWidth: 360, width: 250, position: "relative" }}
       style={{
         boxShadow: "none",
         border: "1px black solid",
@@ -27,19 +27,23 @@ export default function PokemonCard({ pokemon }) {
         alignItems="center"
       >
         <Box
-          display="block"
-          width="40%"
-          height="35%"
-          border-radius="58% 42% 64% 36% / 47% 45% 55% 53%"
-          background-color="red"
-          position="absolute"
-          top="25%"
-          left="30%"
+           sx={{
+    display: "block",
+    width: "12%",
+    height: "13%",
+    borderRadius: "58% 42% 64% 36% / 47% 45% 55% 53%",
+    backgroundColor: "#123456",
+    position: "absolute",
+    top: "12%",
+    left: "27%",
+
+  }}
         />
         <CardMedia
           sx={{ height: 150, width: 150, position: "relative" }}
           image={`${sprites.other["official-artwork"].front_default}`}
           title="green iguana"
+     
         />
       </Box>
       <CardContent>
