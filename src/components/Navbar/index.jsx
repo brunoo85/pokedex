17546f65a802465897b1bc7eddb7f2ef88230e60
Bruno Cesar <brunooufrn@gmail.com>
2025-07-24 04:48:31 -0300

    refactor: pokemon ball on icon

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,8 @@
-import * as React from "react";
 import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import { useNavigate } from "react-router-dom";
 
@@ -55,8 +51,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 export default function Navbar({ pokemonFilter, hideSearch }) {
   const navigate = useNavigate();
   return (
-    <Box sx={{ flexGrow: 1, mb: 2 }}>
-      <AppBar position="static" sx={{ backgroundColor: "black" }}>
+    <Box size={{ flexGrow: 1, mb: 2 }}>
+      <AppBar position="static" sx={{ backgroundColor: "#cc0000" }}>
         <Toolbar>
           <Box
             display="flex"
